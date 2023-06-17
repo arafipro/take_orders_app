b859fea9c3bb9e92533a69cc6a180db855b3b8a5
Hiroki Chiyoda <contact@arafipro.com>
2023-06-17 20:49:08 +0900

    ItemInputPageの修正#43

--- lib/pages/item_input_page.dart
+++ lib/pages/item_input_page.dart
@@ -14,170 +14,162 @@ class ItemInputPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Column(
-            children: [
-              Form(
-                key: _formKey,
+        child: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
                 child: Column(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: TextFormField(
-                        controller: itemNameController,
-                        keyboardType: TextInputType.text,
-                        maxLength: 20,
-                        decoration: InputDecoration(
-                          hintText: "商品名（カタカナ）",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                          ),
+                    TextFormField(
+                      controller: itemNameController,
+                      keyboardType: TextInputType.text,
+                      maxLength: 20,
+                      decoration: InputDecoration(
+                        hintText: "商品名（カタカナ）",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
                         ),
-                        onChanged: (text) {
-                          itemNameController.text = text;
-                        },
-                        // MEMO value!.isEmptyで未入力を警告
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "商品名を入力してください";
-                          } else if (!RegExp(r"^[\u30A0-\u30FF]+$")
-                              .hasMatch(value)) {
-                            return "カタカナで入力してください";
-                          } else {
-                            return null;
-                          }
-                        },
                       ),
+                      onChanged: (text) {
+                        itemNameController.text = text;
+                      },
+                      // MEMO value!.isEmptyで未入力を警告
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "商品名を入力してください";
+                        } else if (!RegExp(r"^[\u30A0-\u30FF]+$")
+                            .hasMatch(value)) {
+                          return "カタカナで入力してください";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    TextFormField(
+                      controller: itemPriceController,
+                      keyboardType: TextInputType.number,
+                      maxLength: 5,
+                      decoration: InputDecoration(
+                        hintText: "価格",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                      onChanged: (text) {
+                        itemPriceController.text = text;
+                      },
+                      // MEMO value!.isEmptyで未入力を警告
+                      // MEMO !RegExp(r"^\d+$").hasMatch(value)で数字以外を警告
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "価格を入力してください";
+                        } else if (!RegExp(r"^\d+$").hasMatch(value)) {
+                          return "数字を入力してください";
+                        } else {
+                          return null;
+                        }
+                      },
                     ),
                     SizedBox(
-                      child: Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: TextFormField(
-                          controller: itemPriceController,
-                          keyboardType: TextInputType.number,
-                          maxLength: 5,
-                          decoration: InputDecoration(
-                            hintText: "価格",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5.0),
-                            ),
-                          ),
-                          onChanged: (text) {
-                            itemPriceController.text = text;
-                          },
-                          // MEMO value!.isEmptyで未入力を警告
-                          // MEMO !RegExp(r"^\d+$").hasMatch(value)で数字以外を警告
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return "価格を入力してください";
-                            } else if (!RegExp(r"^\d+$").hasMatch(value)) {
-                              return "数字を入力してください";
-                            } else {
-                              return null;
-                            }
-                          },
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            await itemRepo.addItem(
+                              itemNameController.text,
+                              int.parse(itemPriceController.text),
+                            );
+                            // MEMO 保存した後に入力を空欄に
+                            itemNameController.text = "";
+                            itemPriceController.text = "";
+                          }
+                        },
+                        child: const Text(
+                          "登録",
                         ),
                       ),
                     ),
                   ],
                 ),
               ),
-              SizedBox(
+            ),
+            Expanded(
+              child: SizedBox(
                 width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      await itemRepo.addItem(
-                        itemNameController.text,
-                        int.parse(itemPriceController.text),
+                child: StreamBuilder<List<Item>>(
+                  stream: itemRepo.watchAllAscItems(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
                       );
-                      // MEMO 保存した後に入力を空欄に
-                      itemNameController.text = "";
-                      itemPriceController.text = "";
                     }
-                  },
-                  child: const Text(
-                    "登録",
-                  ),
-                ),
-              ),
-              Expanded(
-                child: SizedBox(
-                  width: double.infinity,
-                  child: StreamBuilder<List<Item>>(
-                    stream: itemRepo.watchAllAscItems(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                      return SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.vertical,
-                          child: DataTable(
-                            columns: const [
-                              DataColumn(
-                                label: Text(
-                                  "商品名",
-                                  textAlign: TextAlign.center,
-                                ),
+                    return SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: DataTable(
+                          columns: const [
+                            DataColumn(
+                              label: Text(
+                                "商品名",
+                                textAlign: TextAlign.center,
                               ),
-                              DataColumn(
-                                label: Text(
-                                  "価格",
-                                  textAlign: TextAlign.end,
-                                ),
+                            ),
+                            DataColumn(
+                              label: Text(
+                                "価格",
+                                textAlign: TextAlign.end,
                               ),
-                              DataColumn(
-                                label: Text(
-                                  "",
-                                ),
+                            ),
+                            DataColumn(
+                              label: Text(
+                                "",
                               ),
-                            ],
-                            rows: List<DataRow>.generate(
-                              snapshot.data!.length,
-                              (index) => DataRow(
-                                cells: <DataCell>[
-                                  DataCell(
-                                    Text(
-                                      "${snapshot.data![index].itemName}",
-                                    ),
+                            ),
+                          ],
+                          rows: List<DataRow>.generate(
+                            snapshot.data!.length,
+                            (index) => DataRow(
+                              cells: <DataCell>[
+                                DataCell(
+                                  Text(
+                                    "${snapshot.data![index].itemName}",
                                   ),
-                                  DataCell(
-                                    Text(
-                                      "${snapshot.data![index].itemPrice}",
-                                      textAlign: TextAlign.end,
-                                    ),
+                                ),
+                                DataCell(
+                                  Text(
+                                    "${snapshot.data![index].itemPrice}",
+                                    textAlign: TextAlign.end,
                                   ),
-                                  DataCell(
-                                    IconButton(
-                                      icon: Icon(
-                                        Icons.delete,
-                                        color: Colors.red.shade600,
-                                      ),
-                                      onPressed: () async {
-                                        await itemRepo.deleteItem(
-                                          snapshot.data![index].itemId,
-                                        );
-                                      },
+                                ),
+                                DataCell(
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.delete,
+                                      color: Colors.red.shade600,
                                     ),
+                                    onPressed: () async {
+                                      await itemRepo.deleteItem(
+                                        snapshot.data![index].itemId,
+                                      );
+                                    },
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                           ),
                         ),
-                      );
-                    },
-                  ),
+                      ),
+                    );
+                  },
                 ),
               ),
-              AdBanner()
-            ],
-          ),
+            ),
+            AdBanner()
+          ],
         ),
       ),
     );
